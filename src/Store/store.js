9fef38e7124b6bec736a47485a7cd4aec5187e99
JackLuson <18219112108@163.com>
2018-07-19 16:25:57 +0800

    redux-thunk处理异步action

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import * as counternet from './counter/reducers'
 import thunk from 'redux-thunk'
+/* thunk用来处理异步action*/
 let store = createStore(
   combineReducers({
     ...counternet
--- src/index.js
+++ src/index.js
@@ -1,16 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import AppConnected from "./App";
-import { Provider } from "react-redux";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import AppConnected from './App'
+import { Provider } from 'react-redux'
 import store from './Store/store'
-import registerServiceWorker from './registerServiceWorker';
-
+import registerServiceWorker from './registerServiceWorker'
+import { add, subtract } from './Store/counter/action.js'
+let currentValue = store.getState()
+console.log('当前的值:', currentValue)
+// 定义一个监听的方法
+let listener = () => {
+  const previosValue = currentValue
+  currentValue = store.getState()
+  console.log('上一个值:', previosValue, '当前值:', currentValue)
+}
+// 创建一个监听
+store.subscribe(listener)
+// 分发任务
+function addIfOdd () {
+  return (dispatch, getState) => {
+    const currentValue = getState()
+    if (currentValue % 2 == 0) {
+      return false
+    }
+    dispatch(add())
+  }
+}
+function addAsy (delay = 2000) {
+  return (dispatch, getState) => {
+    setInterval(() => {
+      dispatch(add())
+    }, delay)
+  }
+}
+// 分发任务
+store.dispatch(add())
+store.dispatch(add())
+store.dispatch(add())
+store.dispatch(add())
+store.dispatch(subtract())
+store.dispatch(addIfOdd())
+store.dispatch(addAsy())
 const render = Component => {
-  ReactDOM.render(//绑定redux、热加载
+  ReactDOM.render( // 绑定redux、热加载
     <Provider store={store}>
       <Component />
-    </Provider>, document.getElementById("root"));
-};
-render(AppConnected);
-registerServiceWorker();
+    </Provider>, document.getElementById('root'))
+}
+render(AppConnected)
+registerServiceWorker()
